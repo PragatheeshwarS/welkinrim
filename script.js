1b347e60ef44a89586b3607b2c20f3a2c94284e4
S.Pragatheeshwar <101608194+PragatheeshwarS@users.noreply.github.com>
2024-07-02 13:42:33 +0530

    remove back and forth writing to single writing

--- script.js
+++ script.js
@@ -1,26 +1,12 @@
 const writingText = "WELKINRIM TECHNOLOGIES";
 const writingElement = document.getElementById("writing-text");
 
-let index = writingText.length;
-let isDeleting = true;
-
-function writeText() {
-    if (isDeleting) {
-        if (index > 0) {
-            writingElement.textContent = writingElement.textContent.slice(0, -1);
-            index--;
-        } else {
-            isDeleting = false;
-        }
+let index = 0;
+const intervalId = setInterval(() => {
+    if (index < writingText.length) {
+        writingElement.textContent += writingText[index];
+        index++;
     } else {
-        if (index < writingText.length) {
-            writingElement.textContent += writingText[index];
-            index++;
-        } else {
-            isDeleting = true;
-        }
+        clearInterval(intervalId);
     }
-    setTimeout(writeText, 50); // adjust the delay to control the animation speed
-}
-
-writeText();
+}, 80);
